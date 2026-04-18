51a50ccbf0763f06f90a17e2bf92e5f363d1d553
Nick Alekhine <nick.alekhine@gmail.com>
2026-04-18 13:27:11 -0400

    update

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -2,6 +2,8 @@ import { Canvas } from "@react-three/fiber";
 import { Environment, KeyboardControls } from "@react-three/drei";
 import { useRef } from "react";
 import { DumpTruckScene, type DumpTruckHandle } from "./DumpTruckScene";
+import { Ground } from "./Ground";
+import { SunLight } from "./SunLight";
 import { TruckCamera } from "./TruckCamera";
 import { TruckController } from "./TruckController";
 
@@ -21,25 +23,8 @@ export function World() {
         <color attach="background" args={["white"]} />
         <TruckCamera truckRef={truckRef} />
         <Environment preset="city" environmentIntensity={0.5} />
-        <directionalLight
-          castShadow
-          position={[10, 20, 10]}
-          intensity={1.5}
-          shadow-mapSize={[2048, 2048]}
-          shadow-camera-left={-30}
-          shadow-camera-right={30}
-          shadow-camera-top={30}
-          shadow-camera-bottom={-30}
-          shadow-camera-near={0.1}
-          shadow-camera-far={100}
-          shadow-normalBias={0.05}
-          shadow-radius={8}
-          shadow-blurSamples={16}
-        />
-        <mesh receiveShadow rotation-x={-Math.PI / 2}>
-          <planeGeometry args={[1000, 1000]} />
-          <shadowMaterial transparent opacity={0.35} />
-        </mesh>
+        <SunLight truckRef={truckRef} />
+        <Ground truckRef={truckRef} />
         <DumpTruckScene ref={truckRef} position={[0, TRUCK_GROUND_OFFSET, 0]} />
         <TruckController truckRef={truckRef} />
       </Canvas>
